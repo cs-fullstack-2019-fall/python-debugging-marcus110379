65dc8d0228e1bbad9d8059a0ffc596afb831529f
tdude0175 <tdude0175@gmail.com>
2019-09-23 13:20:32 -0500

    graded 2019-09-23

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python_debugging
-
+#### You missed one question. Please Comment your code! 3/5 Score: 3/4
 All of the questions 1 through 10 has bugs in it. Fix each of the questions using the debugger and reading error messages.
--- question5.py
+++ question5.py
@@ -2,5 +2,6 @@
 # Create the list: squad = ["One", "Two", "Three", "Four", "Five"]. Print the list in reverse without using a list method.
 
 squad = ["One", "Two", "Three", "Four", "Five"]
-for index of range(len(squad -1), -1, -1):
-    print(squad(index))
+for index of range(len(squad -1), -1, -1):  #You need to change "of" to "in" for range
+    print(squad(index))  #you need to use bracket [] instead of parenthesis () around index
+    
